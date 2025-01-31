ecca0bc2e59a0419c243e6552cbbf052f7e03194
Apple <apple@Apples-MacBook-Air.local>
2025-02-01 07:27:33 +0900

    [update]deviseとhomeのコントローラとビューを作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root 'home#top'
+  devise_for :users, controllers: { registrations: 'users/registrations', sessions: 'users/sessions' }
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
-
-  # Defines the root path route ("/")
-  # root "posts#index"
+  devise_scope :user do
+    get 'guest_login', to: 'users/sessions#guest_login'
+  end
 end
